722152b3af24389f58d20b2f68b90de0ba51405f
phatxitincut <trangnguyentrungphat@gmail.com>
2018-11-14 16:36:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/trungphat/gsheet_as_database/utils/Keys.java
@@ -0,0 +1,9 @@
+package com.example.trungphat.gsheet_as_database.utils;
+
+public class Keys {
+
+    public static final String KEY_CONTACTS = "Sheet1";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_COUNTRY = "country";
+
+}
